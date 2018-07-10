f8055a7874595e3e503c9d1ab1fe791954840a40
rich-03 <richbel03@icloud.com>
2018-07-10 17:15:07 -0400

    Added Lecture 5.

---
+++ Lecture05/Exercise05-08.py
@@ -0,0 +1,7 @@
+a = 1
+while a <= 8:
+    print(a)
+    a = a + 1
+
+for a in range(1, 9, 1):
+    print(a)
---
+++ Lecture05/Exercise05-10.py
@@ -0,0 +1,5 @@
+# WIP
+for i in range(1, 10):
+    value = int(input("Enter a Positive value."))
+else:
+    print("You have entered", value)
