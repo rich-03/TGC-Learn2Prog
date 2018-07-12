5adc1c7387eab7eca9ca0c027fa59e24d1d4eb93
rich-03 <richbel03@icloud.com>
2018-07-12 14:50:30 -0400

    Added Lecture 6.

---
+++ Lecture06/Exercise06-01-06.py
@@ -0,0 +1,14 @@
+infile = open("data.txt", "r")
+outfile = open("data.txt", "w")
+
+infile.close()
+outfile.close()
+
+with open("infile", "w") as myfile:
+    # Print each line
+
+charles = open("readme.txt", "w")
+# Write out numbers 1-10 per line
+
+outfile = open("data.txt", "w")
+# Find the average of integers listed
