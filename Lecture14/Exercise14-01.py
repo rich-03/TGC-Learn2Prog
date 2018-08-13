148adee8b37084f744160ff7098df38c3af5f1bf
rich-03 <richbel03@icloud.com>
2018-08-13 13:57:02 -0400

    Added Lecture 14.

---
+++ Lecture14/Exercise14-01.py
@@ -0,0 +1,20 @@
+import turtle
+
+wn = turtle.Screen()
+joe = turtle.Turtle()
+
+joe.left(60)
+joe.forward(20)
+joe.right(120)
+joe.forward(10)
+joe.left(60)
+joe.backward(10)
+joe.forward(10)
+joe.right(60)
+joe.forward(10)
+joe.left(60)
+joe.up()
+joe.forward(5)
+joe.down()
+
+input()
